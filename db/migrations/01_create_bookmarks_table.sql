fc0e42b1a7cc0296ad9dfd394124fb8a28d7822d
Paul <paul.ayling@bglgroup.co.uk>
2019-01-28 16:12:24 +0000

    Created database and experimented with SQL commands

--- db/migrations/01_create_bookmarks_table.sql
+++ db/migrations/01_create_bookmarks_table.sql
@@ -1,1 +1,8 @@
 CREATE TABLE bookmarks(id SERIAL PRIMARY KEY, url VARCHAR(60));
+
+
+SELECT * FROM bookmarks;
+
+INSERT INTO bookmarks (url) VALUES ('https://www.google.com');
+INSERT INTO bookmarks (url) VALUES ('https:/www.comparethemarket.com');
+INSERT INTO bookmarks (url) VALUES ('https://www.tesco.com');
